5ee0f588be5230a475d69f7e6c20a8154b732874
shinmorris <shin3298s@gmail.com>
2020-01-20 21:24:52 +0900

    commit3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,19 @@
 $(function(){
-  function buildHTML(){
-  if (message.image){
+  function buildHTML(message){
+  if ( message.image){
     var html =
     `<div class='post-name'>
     <div class='name'>
     ${message.user_name}
     </div>
     <div class='date'>
-    ${message.created_at.strftime('%Y年%m月%d日 %H時%M分')}
+    ${message.created_at}
     </div>
     <div class='post'>
     <p class = 'lower-message__content'>
     ${message.body}
     </p>
-    </div>
+    </div class = 'lower-message__image'>
       <img src=${message.image} >
     </div>
     </div>`
@@ -25,7 +25,7 @@ $(function(){
     ${message.user_name}
     </div>
     <div class='date'>
-    ${message.created_at.strftime("%Y年%m月%d日 %H時%M分")}
+    ${message.created_at}
     </div>
     <div class='post'>
     <p class = "lower-message__content">
@@ -37,24 +37,28 @@ $(function(){
   }
 
   }
-  $('#form_submit').on("submit", function(e){
-  console.log('イベント発火');
+
+  $('.new_message').on("submit", function(e){
   e.preventDefault()
-  var formData = new FormData(this);
+  var formData = new FormData(this)
   var url = $(this).attr('action');
   $.ajax({
     url: url,
     type: "POST",
-    date: formData,
-    datetype: 'json',
+    data: formData,
+    dataType: 'json',
     processData: false,
     contentType: false
   })
   .done(function(data){
     var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    $('.chat-main__message-list').append(html);
+    $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     $('form')[0].reset();
+    $('.form__submit').prop('disabled', false);
   })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+});
   });
 });
