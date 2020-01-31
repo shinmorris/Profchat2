2338b5050ac31f67d3019d3a90ab6fe4c23f6af7
shinmorris <shin3298s@gmail.com>
2020-01-31 16:00:05 +0900

    Update 20200105231907_create_group_users.rb

--- db/migrate/20200105231907_create_group_users.rb
+++ db/migrate/20200105231907_create_group_users.rb
@@ -2,8 +2,8 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.timestamps
-      t.references :group_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
     end
   end
 end
